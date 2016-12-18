6550507f535c90282b681ee5f21f8090a67d5d75
remy-r <remi@perso-cloud.eu>
2016-12-18 15:39:39 +0100

    Delete unused imports

--- programme/makeLog.py
+++ programme/makeLog.py
@@ -2,11 +2,8 @@
 
 import datetime
 import calcul
-import sys
-import os
 
 date_now = datetime.datetime.now()
 ic = date_now.isocalendar()
 
 calcul.write_day(ic[1], date_now.weekday(), date_now.hour, date_now.minute, date_now.hour, date_now.minute)
-
